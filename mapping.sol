80df961437ab5dbeb30d7f10315a0b11af2808f4
Adnan Farooq <m.adnan.far@gmail.com>
2020-04-18 12:38:17 +0500

    Mapping - Reference Data Type

---
+++ mapping.sol
@@ -0,0 +1,41 @@
+pragma solidity^0.6.0;
+
+contract BasicMapping{
+ //Mapping is a key value pair similar to hash table and dictionary in python.  
+ //Enum and Struct can be used as values but not as key.
+ 
+    mapping (uint => string) names;
+    
+    function update() public returns(string memory){
+        names[1] = "John";
+        names[2] = "Mike";
+        return names[2];
+    }
+    
+// function to get values from the user both key and value
+    function updatevalues(uint a, string memory b) public {
+        names[a] = b;
+    }
+    
+    function getvalue(uint a) public view returns(string memory){
+        return names[a];
+    }
+}
+
+contract MyMapping{
+    enum gender {male,female}
+    struct Student{
+        string name;
+        gender MyGender;
+    }
+    
+    mapping (uint =>Student) Students;
+    function addStudent() public{
+        uint rollNo = 1001;
+        Student memory newStudent = Student("Adnan",gender.male);
+        Students[rollNo] = newStudent;
+    }
+    function getStudent(uint rollNo) public view returns(string memory){
+        return Students[rollNo].name;
+    }
+}
