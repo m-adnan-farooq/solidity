e5452bd6611f8b67c839db6dd51c22a6fc2c5aeb
Adnan Farooq <m.adnan.far@gmail.com>
2020-04-18 14:42:36 +0500

    Added sample code for nested mapping

--- mapping.sol
+++ mapping.sol
@@ -39,3 +39,17 @@ contract MyMapping{
         return Students[rollNo].name;
     }
 }
+
+
+
+contract NestedMapping{
+    mapping (string => mapping(int=>string)) stuCourses;
+
+    function addCourse(string memory rollNo, int counter, string memory course) public{
+        stuCourses[rollNo][counter] = course;
+}
+
+    function findCourse(string memory rollNo,int counter) public view returns (string memory){
+        return stuCourses[rollNo][counter];
+    }
+}
